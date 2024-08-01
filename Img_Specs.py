a336b8858858170938f786e184fc1995109581e9
AsafDelmedigo <128504419+delmedigo88@users.noreply.github.com>
2024-08-01 11:04:34 +0300

    Create Img_Specs.py

---
+++ Img_Specs.py
@@ -0,0 +1,5 @@
+class Image_Specifications:
+    def __init__(self, size=1000, dpi=300, is_grayscale=True):
+        self.size = size
+        self.dpi = dpi
+        self.is_grayscale = is_grayscale
