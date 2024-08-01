c667377e9a642593a1ec7428330476e29b40b368
AsafDelmedigo <128504419+delmedigo88@users.noreply.github.com>
2024-08-01 11:06:05 +0300

    Create Maazan.py

---
+++ Maazan.py
@@ -0,0 +1,21 @@
+class MaazanPage(Page):
+    def __init__(self, num, img, data):
+        super().__init__(num, img, data)
+        self.beur_tokens = []
+        self.found_beur_tokens = 0
+        self.find_beur_tokens()
+
+    def find_beur_tokens(self):
+        if self.found_beur_tokens <= 2:
+            for token in self.tokens:
+                if any(str(i) in token.search_text for i in ['אור', 'באור', 'ביאור', 'מידע נוסף', 'נוסף']) and len(token.search_text) < 12:
+                    token.tag = 'beur'
+                    self.beur_tokens.append(token)
+                    self.found_beur_tokens += 1
+
+class Maazan(Document):
+    def __init__(self, path, img_specs, ocr):
+        super().__init__(path, img_specs, ocr)
+
+    def create_page(self, num, img, data):
+        return MaazanPage(num, img, data)
